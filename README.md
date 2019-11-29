428cca65807daa142fed7588d7c955f70a30eba8
Tariq Khan <digitalgnome@users.noreply.github.com>
2019-11-29 06:41:43 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Random Quote Generator]()
+
+A vanilla JavaScript Random Quote generator that uses the JS fetch API to connect to endpoint to retrieve a JSON file that contains 1000 quotes.  The array is then slightly modified to substitute any null author key values with 'Unknown'.  With a 'click' event listener added to a button, a random quote is selected from the array of objects that contain a quote, and author key.  These values are then injected into the HTML document in the form of a Modal overlay.
---
+++ app.js
@@ -0,0 +1,43 @@
+const modalBtn = document.querySelector('#modalBtn');
+const modal = document.querySelector('#modal');
+const h2ModalText = document.querySelector('#modal-text');
+
+// Random quotes API call
+
+const quotesAPIEndpoint = 'https://type.fit/api/quotes';
+
+const getQuote = () => {
+  fetch(quotesAPIEndpoint)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      // Store an array of objects with author and quote
+      const quotes = data.slice(0, 999);
+      generateQuote(quotes);
+    });
+};
+
+// Generate random quote, after replacing null authors with 'Unknown'
+// Inject quote into modal HTML elements
+const generateQuote = quotesArray => {
+  quotesArray.forEach(quote => {
+    if (quote.author === null) {
+      console.log('substitued unknown');
+    }
+  });
+  const quote = quotesArray[Math.floor(Math.random() * 1000)];
+  h2ModalText.innerHTML =
+    quote.text + '<br>- <span>' + quote.author + '</span>';
+};
+
+modalBtn.addEventListener('click', event => {
+  modal.style.display = 'block';
+  getQuote();
+
+  modalBtn.blur();
+});
+
+modal.addEventListener('click', event => {
+  modal.style.display = 'none';
+});
